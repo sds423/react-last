7504114c45b1a66397d280760f30eb12fab6d263
sds423 <salapakadattasaivinay@gmail.com>
2020-08-03 17:07:21 +0530

    Updates

---
+++ precache-manifest.7ca58290a2d6f803b13b3fb0736e4430.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea653c1b9394e28e905ae1d031eca10",
+    "url": "/react-last/index.html"
+  },
+  {
+    "revision": "6bce5a700f007f777dc1",
+    "url": "/react-last/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38635e7a3a02df121e4f",
+    "url": "/react-last/static/js/2.2a290ad0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-last/static/js/2.2a290ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bce5a700f007f777dc1",
+    "url": "/react-last/static/js/main.a3e0a464.chunk.js"
+  },
+  {
+    "revision": "8d764e38b1d86f813249",
+    "url": "/react-last/static/js/runtime-main.0d16a337.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-last/static/media/logo.5d5d9eef.svg"
+  }
+]);
